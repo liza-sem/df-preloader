2f9cc0a08d9a781a5ca48d2e63f6e07f251dc243
liza-sem <168638728+liza-sem@users.noreply.github.com>
2024-05-07 00:04:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ $(document).ready(function() {
         }
     }
 
+    // Ensure exit overlay is hidden on initial load and back navigation
+    $('.exit-overlay').hide();
+
     // Set a timeout to ensure the overlay fades out after a maximum wait time
     setTimeout(function() {
         timeoutComplete = true;
@@ -27,30 +30,30 @@ $(document).ready(function() {
         }
     }, 100);
 
-// Handle click events for navigation links
-$('a').on('click', function(event) {
-    var hasFolderId = $(this).attr('data-folder-id');
-    var isControlLink = $(this).hasClass('header-menu-controls-control');
-    var allowedHrefs = ['#', '#!', '#void', 'javascript:void(0)'];  // Array of allowed hrefs
+    // Handle click events for navigation links
+    $('a').on('click', function(event) {
+        var hasFolderId = $(this).attr('data-folder-id');
+        var isControlLink = $(this).hasClass('header-menu-controls-control');
+        var allowedHrefs = ['#', '#!', '#void', 'javascript:void(0)'];  // Array of allowed hrefs
 
-    // Allow default behavior for these special links or if the href is in the allowed list
-    if (hasFolderId || isControlLink || allowedHrefs.includes($(this).attr('href'))) {
-        return;
-    }
+        // Allow default behavior for these special links or if the href is in the allowed list
+        if (hasFolderId || isControlLink || allowedHrefs.includes($(this).attr('href'))) {
+            return;
+        }
 
-    event.preventDefault();  // Prevent the default link behavior if none of the conditions are met
-    var href = $(this).attr('href');  // Retrieve href here because we need it after preventing default
+        event.preventDefault();  // Prevent the default link behavior if none of the conditions are met
+        var href = $(this).attr('href');  // Retrieve href here because we need it after preventing default
 
-    // Ensure no animation conflicts
-    if (!overlayControlled) {
-        fadeOutOverlay();  // Make sure the overlay fades out if it hasn't yet
-    }
+        // Ensure no animation conflicts
+        if (!overlayControlled) {
+            fadeOutOverlay();  // Make sure the overlay fades out if it hasn't yet
+        }
 
-    $('.exit-overlay').css('display', 'flex').css('opacity', 0).animate({ opacity: 1 }, 500, function() {
-        setTimeout(function() {
-            window.location.href = href; // Redirect after the exit overlay fades in
-        }, 500);
+        $('.exit-overlay').css('display', 'flex').css('opacity', 0).animate({ opacity: 1 }, 500, function() {
+            setTimeout(function() {
+                window.location.href = href; // Redirect after the exit overlay fades in
+            }, 500);
+        });
     });
-});
 
 });
